c7dc4e62c531ada1ba75b9e291268a5c7355f4e3
Breno Tardelli <brenot.dev@gmail.com>
2025-04-16 11:41:58 +0200

    useState added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+
+const messages = ["Learn React 📚", "Apply for jobs 👨‍💻", "Invest your new income 📈"];
 
 const App = () => {
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      setStep(step + 1);
+    }
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div className="active">1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
-      <p className="message">Hello</p>
+      <p className="message">{messages[step - 1]}</p>
 
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>Previous</button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>Next</button>
+        <button onClick={handlePrevious} style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+
+        <button onClick={handleNext} style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
       </div>
     </div>
   );
