e675683f78caddbd63322efe4a5fc8391f670627
Breno Tardelli <brenot.dev@gmail.com>
2025-04-16 20:40:00 +0200

    close button  added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ const messages = ["Learn React 📚", "Apply for jobs 👨‍💻", "Invest your
 
 const App = () => {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
     if (step > 1) {
@@ -18,24 +19,31 @@ const App = () => {
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
-      <p className="message">{messages[step - 1]}</p>
+    <>
+      <button onClick={() => setIsOpen((is) => !is)} className="close">
+        {isOpen ? "x" : "open"}
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+          <p className="message">{messages[step - 1]}</p>
 
-      <div className="buttons">
-        <button onClick={handlePrevious} style={{ backgroundColor: "#7950f2", color: "#fff" }}>
-          Previous
-        </button>
+          <div className="buttons">
+            <button onClick={handlePrevious} style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+              Previous
+            </button>
 
-        <button onClick={handleNext} style={{ backgroundColor: "#7950f2", color: "#fff" }}>
-          Next
-        </button>
-      </div>
-    </div>
+            <button onClick={handleNext} style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
